bc86e06a12d4ef3d3a58828a573069eb7fee17f4
Gabriel Sampaio <gasampaio.souza@gmail.com>
2021-04-13 12:46:09 -0300

    update code a little :tada;

--- code/checkout/BirthdayField/requiredAndMoreThan18.js
+++ code/checkout/BirthdayField/requiredAndMoreThan18.js
@@ -1,24 +1,38 @@
+/*
+  ----
+  YOU WILL NEED JQUERY MASK PLUGIN!!
+  > to remove the mask, go to line 221
+  ----
+*/
+
+// set minimum age
+const MINIMUM_AGE = 18;
+const _headers = {
+  Accept: 'application/vnd.vtex.ds.v10+json',
+  'Content-Type': 'application/json',
+};
+
 const funcsBirthDate = {
   init: () => {
       const userData = vtexjs.checkout.orderForm.clientProfileData;
-      //EXISTE DUAS POSSIBILIDADES DE USUÁRIOS LOGADOS
-      //1° TEM DADOS SALVOS MAS NÃO ESTÁ CONECTADO
-      //2° TEM DADOS SALVOS E ESTÁ CONECTADO
-      //3° NÃO TEM DADOS SALVOS
+      //EXISTE DUAS POSSIBILIDADES DE USUÃRIOS LOGADOS
+      //1Â° TEM DADOS SALVOS MAS NÃƒO ESTÃ CONECTADO
+      //2Â° TEM DADOS SALVOS E ESTÃ CONECTADO
+      //3Â° NÃƒO TEM DADOS SALVOS
 
-      //VERIFICAÇÃO SE EXISTE DADOS DO USUÁRIO
+      //VERIFICAÃ‡ÃƒO SE EXISTE DADOS DO USUÃRIO
       if (userData !== null) {
           if (userData.document !== null && userData.document.split('*').length > 1 ||
               userData.firstName !== null && userData.firstName.split('*').length > 1 ||
               userData.phone !== null && userData.phone.split('*').length > 1) {
-              //1°
+              //1Â°
               funcsBirthDate.checKIfHaveBirthDate(1);
           } else {
-              //2°
+              //2Â°
               funcsBirthDate.checKIfHaveBirthDate(2);
           }
       } else {
-          //3°
+          //3Â°
           funcsBirthDate.checKIfHaveBirthDate(3);
       }
   },
@@ -45,15 +59,17 @@ const funcsBirthDate = {
                   if (user.length > 0) {
                       //EMAIL CADASTRADO
                       var _user_id = user[0].id;
-                      var _date = (user[0].birthDate !== null && user[0].birthDate !== undefined ? user[0].birthDate.split('T')[0] : '');
+                      var [year, month, day] = (user[0].birthDate !== null && user[0].birthDate !== undefined ? user[0].birthDate.split('T')[0] : '').split('-');
+                      const _date = `${day}/${month}/${year}`
+                      
                       $('#client-birthDate').val(_date);
-                      console.log('Email está cadastrado')
+
+                      console.log('Email estÃ¡ cadastrado')
                       const userAge = funcsBirthDate.getAge(_date);
                     
-                      
-                      if (_date === '' || userAge < 18) {
-                          //NÃO POSSUI DATA DE NASCIMENTO OU É MENOR DE 18
-                          //FORÇA USUÁRIO PREENCHER DATA DE NASCIMENTO OU SAIR
+                      if (_date === '' || userAge < MINIMUM_AGE) {
+                          //NÃƒO POSSUI DATA DE NASCIMENTO OU Ã‰ MENOR DE 18
+                          //FORÃ‡A USUÃRIO PREENCHER DATA DE NASCIMENTO OU SAIR
                           if (option === 1) {
                               window.location.hash !== '#/profile' ? $('.link-box-edit.btn.btn-small').click() : null;
 
@@ -77,7 +93,7 @@ const funcsBirthDate = {
                       }
                       funcsBirthDate.actionToSaveOrCreate();
                   } else {
-                      //EMAIL NÃO CADASTRADO
+                      //EMAIL NÃƒO CADASTRADO
                       window.location.hash !== '#/profile' ? window.location.hash = '#/profile' : null;
 
                       funcsBirthDate.actionToSaveOrCreate();
@@ -95,7 +111,7 @@ const funcsBirthDate = {
       const createError = (error) => {
         $('.client-birth .help.error').remove()
         $('#client-birthDate').after(`<span class="help error" style="width: 100%">${error}</span>`);
-        $('#client-birthDate').focus();
+        // $('#client-birthDate').focus();
         $('#client-birthDate').addClass('error');
       }
 
@@ -109,56 +125,57 @@ const funcsBirthDate = {
           const userAge = funcsBirthDate.getAge(userBirthday);
 
           if ($('#client-birthDate').val().length < 10) {
-              //DATA DE NASCIMENTO NÃO PREENCHIDA
+              //DATA DE NASCIMENTO NÃƒO PREENCHIDA
 
-              createError('Campo obrigatório.')
+              createError('Campo obrigatÃ³rio.')
           } else if (!isOfAge(userAge)) {
-              createError('Você deve ter mais de 18 anos!')
+              createError('VocÃª deve ter mais de 18 anos!')
           } else {
               $('.client-birth .help.error').remove()
           }
 
-          var _element = $(this);
-          var _date = _element.val().replace(/\//g, '-');
-          var _user_id = _element.attr('data-id');
+          const _element = $(this);
+          const [day, month, year] = _element.val().split('/');
+          const _date = `${year}-${month}-${day}`;
+          const _user_id = _element.attr('data-id');
+
           if (_user_id !== "undefined") {
               funcsBirthDate.saveData(_date, _user_id);
           } else {
               funcsBirthDate.createData(_date);
           }
-
-
       });
 
-      $('#go-to-shipping').click(function (e) {
+      $('#go-to-shipping').on('click', function (e) {
           const userBirthday = $('#client-birthDate').val();
           const userAge = funcsBirthDate.getAge(userBirthday);
 
           if (userBirthday.length < 10) {
               e.preventDefault();
               
-              createError('Campo obrigatório.')
+              createError('Campo obrigatÃ³rio.')
           } else if (!isOfAge(userAge)) {
             e.preventDefault();
 
-            createError('Você deve ter mais de 18 anos!')
+            createError('VocÃª deve ter mais de 18 anos!')
           }
       })
   },
   createData: function (_date) {
       if (_date != "" && _date != undefined && _date != null) {
-          var _email = $('#client-email').val();
-          var _json = {
+          const _email = $('#client-email').val();
+          const _json = {
               "email": _email,
               "birthDate": _date
           };
+
           $.ajax({
               headers: _headers,
               data: JSON.stringify(_json),
               type: 'PATCH',
               url: '/api/dataentities/CL/documents/',
               success: function (user) {
-                  //CRIOU USUÁRIO JUNTO COM DATA DE NASCIMENTO
+                  //CRIOU USUÃRIO JUNTO COM DATA DE NASCIMENTO
               }
           });
       }
@@ -180,33 +197,31 @@ const funcsBirthDate = {
       }
   },
 
-  getAge: birth => {
-    let today = new Date();
-    ageMS = Date.parse(Date()) - Date.parse(birth);
-    today.setTime(ageMS);
-    age = today.getFullYear() - 1970;
-  
-    return age || 0;
-  },
+  getAge: (birth) => {
+    const [day, month, year] = birth.split("/");
+    const birthday = new Date(parseInt(year, 10),
+    parseInt(month, 10) - 1,
+    parseInt(day, 10));
+
+    const diff = Date.now() - birthday.getTime();
+    const age = new Date(diff);
+
+    return Math.abs(age.getUTCFullYear() - 1970) || 0;
+},
 
   addFieldBirthDate: (_date, _user_id) => {
       const fieldToAppendAfter = $($('.client-email')[1])
       if (!$('p.client-birth').length) {
           var ua = navigator.userAgent.toLowerCase();
           if (ua.indexOf('safari') != -1) {
-              if (ua.indexOf('chrome') > -1) {
-                  fieldToAppendAfter.after('<p class="client-birth input pull-left text mask" style="margin-left: 18px; width: 48%;">    <label for="client-birthDate">Data de nascimento</label>    <input type="date" id="client-birthDate" class="input-small" value="' + _date + '" data-id="' + _user_id + '"> </p>');
-              } else {
-                  fieldToAppendAfter.after('<p class="client-birth input pull-left text mask" style="margin-left: 18px; width: 48%;">    <label for="client-birthDate">Data de nascimento</label>    <input type="text" id="client-birthDate" class="input-small" value="' + _date + '" data-id="' + _user_id + '" placeholder="DD/MM/AAAA"> </p>');
-                  try {
-                      $('#client-birthDate').mask('99/99/9999');
+            fieldToAppendAfter.after(`
+                <p class="client-birth input text mask">
+                    <label for="client-birthDate">Data de nascimento</label>
+                    <input type="text" id="client-birthDate" class="input-small" value="${_date}" data-id="${_user_id}" placeholder="dd/mm/aaaa" />
+                </p>`);
 
-                  } catch (error) {
-                      console.log(error);
-                  }
-              }
-
-              fieldToAppendAfter.addClass('pull-left').css({ width: '48%' });
+              // Jquery mask plugin
+              $('#client-birthDate').mask('00/00/0000');
           }
 
       };
@@ -231,6 +246,4 @@ $(document).ready(function () {
       ? funcsBirthDate.init()
       : null;
   }, 1200);
-
-  saveTimeStampFromBuying();
 });
